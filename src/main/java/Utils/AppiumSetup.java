e1cb055eb93d7d1b69e8599ab4ea1116bf9e28e0
vigo <victor.gonzalez@brokergenius.com>
2020-05-13 18:08:38 +0200

    add appium setup

---
+++ src/main/java/Utils/AppiumSetup.java
@@ -0,0 +1,45 @@
+package Utils;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import java.io.File;
+
+public class AppiumSetup {
+
+    public static AppiumDriverLocalService service;
+    public static AndroidDriver<WebElement> driver;
+
+    public void init() {
+
+        File apkWallapop = new File("/home/vigo/Documents/king_test/kingTest/src/main/java/Utils/Apps/wallapop-1-95-1.apk");
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 30);
+        caps.setCapability(MobileCapabilityType.APP, apkWallapop.getAbsolutePath());
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.1.0");
+        caps.setCapability(MobileCapabilityType.UDID, "WE004149");
+
+        try {
+            AppiumServiceBuilder start;
+            start = new AppiumServiceBuilder().withCapabilities(caps).withIPAddress("127.0.0.1").usingPort(4723);
+            service = start.build();
+            service.start();
+
+            driver = new AndroidDriver<WebElement>(service.getUrl(), caps);
+
+        }catch (Exception e){
+            System.out.println("Error connecting Appium: " + e.getMessage());
+        }
+    }
+
+    public void close() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
