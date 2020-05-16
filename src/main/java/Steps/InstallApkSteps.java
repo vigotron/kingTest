7c5cb38a748a00828685746295990fd6e914f927
vigo <victor.gonzalez@brokergenius.com>
2020-05-16 18:07:18 +0200

    add exception

--- src/main/java/Steps/InstallApkSteps.java
+++ src/main/java/Steps/InstallApkSteps.java
@@ -3,10 +3,12 @@ package Steps;
 import Utils.InstallApk;
 import cucumber.api.java.en.Given;
 
+import java.io.IOException;
+
 public class InstallApkSteps {
 
     @Given("Wallapop apk has been installed and launched")
-    public void installAndLaunchWallapop() {
+    public void installAndLaunchWallapop() throws IOException {
         InstallApk installWallapop = new InstallApk();
         installWallapop.installWallapop();
     }
