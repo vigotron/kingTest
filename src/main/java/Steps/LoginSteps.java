f1e7c3d1c12c105822fcd1d2c7cffe8da0a4e810
vigo <victor.gonzalez@brokergenius.com>
2020-05-16 18:08:41 +0200

    add properties file to login

--- src/main/java/Steps/LoginSteps.java
+++ src/main/java/Steps/LoginSteps.java
@@ -1,42 +1,46 @@
 package Steps;
 
+import Pages.HomeScreen;
 import Pages.Login;
 import Utils.AppiumSetup;
 import cucumber.api.java.After;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class LoginSteps {
 
     @And("the user has opened pre-login page")
     public void theUserHasOpenedPreLoginPage() {
         Login preloginPageIsOpen = new Login();
-        preloginPageIsOpen.preloginPageIsOpen();
+        Assert.assertTrue(preloginPageIsOpen.preloginPageIsOpen());
     }
-
     @And("the user has selected Continue with Facebook")
     public void theUserSelectsButtonContinueWithFacebook() {
         Login continueFacebookLogin = new Login();
         continueFacebookLogin.continueFacebookLogin();
     }
-
     @When("the user types valid Facebook credentials and taps login button")
-    public void theUserTypesValidFacebookCredentialsAndTapsLoginButton() {
-        String username = "vigotest78@gmail.com";
-        String pass = "VigoTest_2020";
+    public void theUserTypesValidFacebookCredentialsAndTapsLoginButton() throws IOException {
+        Properties loadUserData = new Properties();
+        loadUserData.load(new FileInputStream("env.properties"));
+        String userName = loadUserData.getProperty("userEmail");
+        String userPass = loadUserData.getProperty("userPass");
         Login loginWithFacebook = new Login();
-        loginWithFacebook.loginWithFacebook(username,pass);
+        loginWithFacebook.loginWithFacebook(userName, userPass);
     }
-
     @Then("the user will be successfully logged in")
     public void theUserWillBeSuccessfullyLoggedIn() {
-        Login userIsLoggedIn = new Login();
-        assert userIsLoggedIn.userIsLoggedIn();
+        HomeScreen userIsLoggedIn = new HomeScreen();
+        Assert.assertTrue(userIsLoggedIn.userIsLoggedIn());
     }
-
     @After
-    public void close(){
+    public void close() {
         AppiumSetup appiumSetup = new AppiumSetup();
         appiumSetup.close();
     }
