65e1c80784ff1654925db85d8b825fbe53cda017
vigo <victor.gonzalez@brokergenius.com>
2020-05-16 12:32:57 +0200

    naming refactor add ids to constructor

--- src/main/java/Pages/Login.java
+++ src/main/java/Pages/Login.java
@@ -6,50 +6,53 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.util.List;
 
 public class Login extends AppiumSetup {
 
+    private final String continueWithFacebookButtonId = "com.wallapop:id/facebookButton";
+    private final String registerWallapopButtonId = "com.wallapop:id/registerButton";
+    private final String skipWallapopTutorialButtonId = "com.wallapop:id/skip";
+    private final String acceptTosButtonId = "com.wallapop:id/button_agree";
+    private final String wallapopProfileImageId = "com.wallapop:id/wp__profile_frame__iv_profile_thumb";
+
     public void preloginPageIsOpen() {
-        MobileElement facebookLoginButton = (MobileElement) driver.findElementById("com.wallapop:id/facebookButton");
-        MobileElement googleLoginButton = (MobileElement) driver.findElementById("com.wallapop:id/googleButton");
-        MobileElement wallapopRegisterButton = (MobileElement) driver.findElementById("com.wallapop:id/registerButton");
-        assert facebookLoginButton.isDisplayed();
-        assert googleLoginButton.isDisplayed();
+        MobileElement wallapopRegisterButton = (MobileElement) driver.findElementById(registerWallapopButtonId);
         assert wallapopRegisterButton.isDisplayed();
     }
 
     public void continueFacebookLogin() {
-        MobileElement facebookLoginOption = (MobileElement) driver.findElementById("com.wallapop:id/facebookButton");
-        facebookLoginOption.click();
+        MobileElement continueWithFacebookLogin = (MobileElement) driver.findElementById(continueWithFacebookButtonId);
+        continueWithFacebookLogin.click();
     }
 
     public void loginWithFacebook(String username, String password) {
         WebDriverWait wait = new WebDriverWait(driver, 15);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("android.widget.Button")));
 
-        List<WebElement> fbUserNameField1 = driver.findElementsByClassName("android.widget.EditText");
-        fbUserNameField1.get(0).sendKeys(username);
-        fbUserNameField1.get(1).sendKeys(password);
-        MobileElement fbUserLoginButton = (MobileElement) driver.findElementByClassName("android.widget.Button");
-        fbUserLoginButton.click();
+        List<WebElement> facebookCredentialFields = driver.findElementsByClassName("android.widget.EditText");
+        facebookCredentialFields.get(0).sendKeys(username);
+        facebookCredentialFields.get(1).sendKeys(password);
+        MobileElement facebookLoginButton = (MobileElement) driver.findElementByClassName("android.widget.Button");
+        facebookLoginButton.click();
 
-        WebDriverWait waitToBeClickable = new WebDriverWait(driver,15);
+        WebDriverWait waitToBeClickable = new WebDriverWait(driver, 15);
         waitToBeClickable.until(ExpectedConditions.elementToBeClickable(By.className("android.widget.Button")));
-        MobileElement continueLogin = (MobileElement) driver.findElementByClassName("android.widget.Button");
-        continueLogin.click();
+        MobileElement continueAsFacebookUser = (MobileElement) driver.findElementByClassName("android.widget.Button");
+        continueAsFacebookUser.click();
 
-        MobileElement skipWallapopIntro = (MobileElement) driver.findElementById("com.wallapop:id/skip");
+        MobileElement skipWallapopIntro = (MobileElement) driver.findElementById(skipWallapopTutorialButtonId);
         skipWallapopIntro.click();
-        MobileElement acceptTermsOfService = (MobileElement) driver.findElementById("com.wallapop:id/button_agree");
+        MobileElement acceptTermsOfService = (MobileElement) driver.findElementById(acceptTosButtonId);
         acceptTermsOfService.click();
     }
 
     public boolean userIsLoggedIn() {
         WebDriverWait wait = new WebDriverWait(driver, 15);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.wallapop:id/wp__profile_frame__iv_profile_thumb")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(wallapopProfileImageId)));
 
-        MobileElement userProfileImage = (MobileElement) driver.findElementById("com.wallapop:id/wp__profile_frame__iv_profile_thumb");
-        return userProfileImage.isDisplayed();
+        MobileElement wallapopProfileImage = (MobileElement) driver.findElementById(wallapopProfileImageId);
+        return wallapopProfileImage.isDisplayed();
     }
 }
