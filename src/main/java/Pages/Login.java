0e583452bfe9bd571717a8d3d5500f2834ea5f2c
vigo <victor.gonzalez@brokergenius.com>
2020-05-16 18:06:15 +0200

    user logged in to new class

--- src/main/java/Pages/Login.java
+++ src/main/java/Pages/Login.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.List;
 
 public class Login extends AppiumSetup {
@@ -15,21 +14,18 @@ public class Login extends AppiumSetup {
     private final String registerWallapopButtonId = "com.wallapop:id/registerButton";
     private final String skipWallapopTutorialButtonId = "com.wallapop:id/skip";
     private final String acceptTosButtonId = "com.wallapop:id/button_agree";
-    private final String wallapopProfileImageId = "com.wallapop:id/wp__profile_frame__iv_profile_thumb";
 
-    public void preloginPageIsOpen() {
+    public boolean preloginPageIsOpen() {
         MobileElement wallapopRegisterButton = (MobileElement) driver.findElementById(registerWallapopButtonId);
-        assert wallapopRegisterButton.isDisplayed();
+        return wallapopRegisterButton.isDisplayed();
     }
-
     public void continueFacebookLogin() {
         MobileElement continueWithFacebookLogin = (MobileElement) driver.findElementById(continueWithFacebookButtonId);
         continueWithFacebookLogin.click();
     }
-
     public void loginWithFacebook(String username, String password) {
-        WebDriverWait wait = new WebDriverWait(driver, 15);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("android.widget.Button")));
+        WebDriverWait waitToDisplay = new WebDriverWait(driver, 15);
+        waitToDisplay.until(ExpectedConditions.visibilityOfElementLocated(By.className("android.widget.Button")));
 
         List<WebElement> facebookCredentialFields = driver.findElementsByClassName("android.widget.EditText");
         facebookCredentialFields.get(0).sendKeys(username);
@@ -46,13 +42,5 @@ public class Login extends AppiumSetup {
         skipWallapopIntro.click();
         MobileElement acceptTermsOfService = (MobileElement) driver.findElementById(acceptTosButtonId);
         acceptTermsOfService.click();
-    }
-
-    public boolean userIsLoggedIn() {
-        WebDriverWait wait = new WebDriverWait(driver, 15);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(wallapopProfileImageId)));
-
-        MobileElement wallapopProfileImage = (MobileElement) driver.findElementById(wallapopProfileImageId);
-        return wallapopProfileImage.isDisplayed();
     }
 }
