26f478bec0f020b3b00ab86b33780ad434d7f974
vigo <victor.gonzalez@brokergenius.com>
2020-05-16 18:09:04 +0200

    add properties file

--- src/main/java/Utils/AppiumSetup.java
+++ src/main/java/Utils/AppiumSetup.java
@@ -6,24 +6,31 @@ import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class AppiumSetup {
 
     public static AppiumDriverLocalService service;
     public static AndroidDriver<WebElement> driver;
 
-    public void init() {
+    public void init() throws IOException {
 
+        Properties loadDeviceProps = new Properties();
+        loadDeviceProps.load(new FileInputStream("env.properties"));
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 30);
         caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
         caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.1.0");
-        caps.setCapability(MobileCapabilityType.UDID, "WE004149");
+        caps.setCapability(MobileCapabilityType.UDID, loadDeviceProps.getProperty("udid"));
 
         try {
             AppiumServiceBuilder start;
-            start = new AppiumServiceBuilder().withCapabilities(caps).withIPAddress("127.0.0.1").usingPort(4723);
+            start = new AppiumServiceBuilder().withCapabilities(caps);
+            start.withIPAddress(loadDeviceProps.getProperty("url"));
+            start.usingPort(Integer.parseInt(loadDeviceProps.getProperty("port")));
             service = start.build();
             service.start();
             driver = new AndroidDriver<WebElement>(service.getUrl(), caps);
@@ -31,7 +38,6 @@ public class AppiumSetup {
             System.out.println("Error connecting Appium: " + e.getMessage());
         }
     }
-
     public void close() {
         if (driver != null) {
             driver.quit();
